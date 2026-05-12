35eea2ee12ca9ace0bb3bcff1e6bb3c67e8d6348
JuxxxWildan10 <yarzuqwildani168@gmail.com>
2026-05-12 10:36:05 +0700

    fix: use any cast for next-pwa require to bypass bundler moduleResolution exports check

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 // eslint-disable-next-line @typescript-eslint/no-require-imports
-const withPWA = (require("next-pwa") as typeof import("next-pwa"))({
+const withPWA = (require("next-pwa") as any)({
   dest: "public",
   register: true,
   skipWaiting: true,
